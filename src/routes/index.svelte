92c6b36318cacb6d794c17710a6283f291fa7b80
Josh Nussbaum <joshnuss@gmail.com>
2022-06-15 01:05:00 -0400

    Add example of switching plan without Stripe portal, just a single click

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,12 @@
 {#if !$auth.loading}
   {#if $auth.user}
     <p>Welcome <b>{$auth.name}</b>! You're on the <b>{$auth.product}</b> plan.</p>
+
+    {#if $auth.audience == 'basic'}
+      <p>
+        Special offer. Upgrade to the <a href="https://id.app.local:3002/account/switch/pro">PRO plan</a>!
+      </p>
+    {/if}
   {:else}
     Good day, would you like to <a href="https://id.app.local:3002/signup">Signup</a>?
   {/if}
